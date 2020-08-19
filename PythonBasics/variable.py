7e61ecfa6427c6ad7483fb57be841b022357392d
Pallab Panda <Pallab.Panda@gmail.com>
2020-08-19 18:17:17 +0530

    variable , string , user input

---
+++ PythonBasics/variable.py
@@ -0,0 +1,15 @@
+x = 1500
+PI = 3.14
+
+result = PI * 7.2
+print(result)
+
+name = "pallab"  # string variable
+
+# Excersie
+
+var1 = "panda"
+var2 = "panda"
+num1 = 4
+num2 = 4
+print(num1 * num2)
